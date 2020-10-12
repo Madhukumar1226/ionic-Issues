25b89456271a512e4a8153e3aee3ae568e67079e
Madhukumar1226 <madhukumar1226@gmail.com>
2020-10-12 12:02:24 +0530

    Added Log

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -14,26 +14,26 @@ export class HomePage {
   ) {
 
   }
-  
+
   ngOnInit(): void {
     this.http.get('https://ionicframework.com/docs/api/loading').subscribe(
       res => {
-        console.log(res)
+        console.log("Response 1 ", res);
       }
     );
     this.http.get('https://ionicframework.com/docs/api/loading').subscribe(
       res => {
-        console.log(res)
+        console.log("Response 2 ", res);
       }
     )
     this.http.get('https://ionicframework.com/docs/api/loading').subscribe(
       res => {
-        console.log(res)
+        console.log("Response 3 ", res);
       }
     )
     this.http.get('https://ionicframework.com/docs/api/loading').subscribe(
       res => {
-        console.log(res)
+        console.log("Response 4 ", res);
       }
     )
   }
